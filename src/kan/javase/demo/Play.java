59a5d2d2430cb188ec77de9fc2686f9eb86c263c
kanxingwang <kanxw@missfresh.cn>
2017-09-26 11:28:16 +0800

    f
    Signed-off-by: kanxingwang <kanxw@missfresh.cn>

---
+++ src/kan/javase/demo/Play.java
@@ -0,0 +1,22 @@
+package kan.javase.demo;
+/**
+ * 和动物一起玩
+ * @author Administrator
+ *
+ */
+public class Play {
+	
+	 public static void  play(Animal animal) {
+		 animal.eat();
+		 animal.sleep();
+	}
+	
+	public static void main(String[] args) {
+		Animal dog=new Dog();
+		Animal cat=new Cat();
+		play(dog);
+		play(cat);
+		
+	}
+
+}
---
+++ src/kan/javase/jdbc/MysqlConn.java
@@ -0,0 +1,28 @@
+package kan.javase.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MysqlConn {
+	public String name="com.mysql.jdbc.Driver";
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection connection=DriverManager.getConnection(  "jdbc:mysql://localhost:3306/test","root","1234");
+			Statement stmt=connection.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from people");  
+			while(rs.next())  
+			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			connection.close();  
+		} catch (Exception  e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+
+}
