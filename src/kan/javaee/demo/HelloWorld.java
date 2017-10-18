daabc8efc02b7c098e709bb8b7bb2882f8fa20fe
kanxingwang <kanxw@missfresh.cn>
2017-10-18 15:23:47 +0800

    f
    Signed-off-by: kanxingwang <kanxw@missfresh.cn>

--- src/kan/javaee/demo/HelloWorld.java
+++ src/kan/javaee/demo/HelloWorld.java
@@ -2,6 +2,7 @@ package kan.javaee.demo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorld
+ * first Servlet 
  */
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
@@ -21,6 +22,7 @@ public class HelloWorld extends HttpServlet {
 		response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>Hello World</title></head>");
+        out.println("<body><h1>Hello World <h1><h2>"  + "</h2></body></html>");
         out.flush();
 	}
 
